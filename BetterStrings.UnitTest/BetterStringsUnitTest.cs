5252935bc0913ce72b22a7cbbf50c7939b39a346
Dan Nemec <djnemec@gmail.com>
2012-12-06 01:30:08 -0600

    First commit, with Dictionary/String formatting. Allow passing a dictionary of keywords to a string Format extension instead of using positional parameters.

---
+++ BetterStrings.UnitTest/BetterStringsUnitTest.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// ReSharper disable CheckNamespace
+namespace BetterStrings.UnitTest
+// ReSharper restore CheckNamespace
+{
+    [TestClass]
+    public class BetterStringsExtensionsUnitTest
+    {
+        [TestMethod]
+        public void FormatDict_WithSingleKey()
+        {
+            var actual = "{hello}".Format(new Dictionary<string, object>
+                {
+                    {"hello", 4}
+                });
+            Assert.AreEqual("4", actual);
+        }
+
+        [TestMethod]
+        public void Format_WithFormatString()
+        {
+            var actual = "{hello:yyyy}".Format(new Dictionary<string, object>
+                {
+                    {"hello", new DateTime(1999, 1, 1)}
+                });
+            Assert.AreEqual("1999", actual);
+        }
+
+        [TestMethod]
+        public void Format_WithDoubledBrace()
+        {
+            var actual = "{{ {hello:yyyy}".Format(new Dictionary<string, object>
+                {
+                    {"hello", new DateTime(1999, 1, 1)}
+                });
+            Assert.AreEqual("{ 1999", actual);
+        }
+
+        [TestMethod]
+        public void Format_WithSideBySideBraces()
+        {
+            var actual = "{{{hello:yyyy}".Format(new Dictionary<string, object>
+                {
+                    {"hello", new DateTime(1999, 1, 1)}
+                });
+            Assert.AreEqual("{1999", actual);
+        }
+
+        [TestMethod]
+        public void Format_WithOpenAndCloseBraces()
+        {
+            var actual = "{{{hello:yyyy}}}{{}}".Format(new Dictionary<string, object>
+                {
+                    {"hello", new DateTime(1999, 1, 1)}
+                });
+            Assert.AreEqual("{1999}{}", actual);
+        }
+
+        [TestMethod]
+        public void Format_WithAlignment()
+        {
+            var actual = "{hello,-6}".Format(new Dictionary<string, object>
+                {
+                    {"hello", 4}
+                });
+            Assert.AreEqual("4     ", actual);
+        }
+
+        [TestMethod]
+        public void Format_WithAlignmentAndFormatString()
+        {
+            var actual = "{hello,-10:yyyy}".Format(new Dictionary<string, object>
+                {
+                    {"hello", new DateTime(1999, 1, 1)}
+                });
+            Assert.AreEqual("1999      ", actual);
+        }
+
+        [TestMethod]
+        public void Format_WithSpaces()
+        {
+            var actual = "{ \nhello\t :yyyy}".Format(new Dictionary<string, object>
+                {
+                    {"hello", new DateTime(1999, 1, 1)}
+                });
+            Assert.AreEqual("1999", actual);
+        }
+
+        [TestMethod]
+        public void Format_WithExtraParameters_DoesNotThrowException()
+        {
+            var actual = "{hello}".Format(new Dictionary<string, object>
+                {
+                    {"hello", 10},
+                    {"something", "oh"}
+                });
+            Assert.AreEqual("10", actual);
+        }
+
+        [TestMethod]
+        public void Format_WithParametersOutOfOrder_FormatsCorrectParameters()
+        {
+            var actual = "{hello}{world}".Format(new Dictionary<string, object>
+                {
+                    {"world", 10},
+                    {"hello", "oh"}
+                });
+            Assert.AreEqual("oh10", actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void Format_WithNoClosingBrace_ThrowsFormatException()
+        {
+            "{hello:yyyy".Format(new Dictionary<string, object>
+                {
+                    {"hello", new DateTime(1999, 1, 1)}
+                });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void Format_WithNoOpeningBrace_ThrowsFormatException()
+        {
+            "hello:yyyy}".Format(new Dictionary<string, object>
+                {
+                    {"hello", new DateTime(1999, 1, 1)}
+                });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void Format_WithMissingParameter_ThrowsFormatException()
+        {
+            "{hello}".Format(new Dictionary<string, object>
+                {
+                    {"rodney", new DateTime(1999, 1, 1)}
+                });
+        }
+    }
+}
---
+++ BetterStrings/BetterStrings.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+// ReSharper disable CheckNamespace
+// ReSharper disable PartialTypeWithSinglePart
+
+namespace BetterStrings
+{
+    /// <summary>
+    /// Adds various string extensions
+    /// </summary>
+    public static partial class BetterStrings
+    {
+        #region Builtin string formatting overrides
+
+        /// <summary>
+        /// Format the input string with <param name="arg" /> as
+        /// its sole parameter.
+        /// </summary>
+        /// <param name="fmt">Format string.</param>
+        /// <param name="arg">Parameter for index 0.</param>
+        /// <returns>Formatted string.</returns>
+        public static string Format(this string fmt, object arg)
+        {
+            return String.Format(fmt, arg);
+        }
+
+        /// <summary>
+        /// Format the input string with two parameters.
+        /// </summary>
+        /// <param name="fmt">Format string.</param>
+        /// <param name="arg0">Parameter for index 0.</param>
+        /// <param name="arg1">Parameter for index 1.</param>
+        /// <returns>Formatted string.</returns>
+        public static string Format(this string fmt, object arg0, object arg1)
+        {
+            return String.Format(fmt, arg0, arg1);
+        }
+
+        /// <summary>
+        /// Format the input string with three parameters.
+        /// </summary>
+        /// <param name="fmt">Format string.</param>
+        /// <param name="arg0">Parameter for index 0.</param>
+        /// <param name="arg1">Parameter for index 1.</param>
+        /// <param name="arg2">Parameter for index 2.</param>
+        /// <returns>Formatted string.</returns>
+        public static string Format(this string fmt, object arg0, object arg1, object arg2)
+        {
+            return String.Format(fmt, arg0, arg1, arg2);
+        }
+
+        /// <summary>
+        /// Format the input string with a variable number
+        /// of parameters.
+        /// </summary>
+        /// <param name="fmt">Format string.</param>
+        /// <param name="args">Positional arguments.</param>
+        /// <returns>Formatted string.</returns>
+        public static string Format(this string fmt, params object[] args)
+        {
+            return String.Format(fmt, args);
+        }
+
+        /// <summary>
+        /// Format the input string with a variable number of parameters,
+        /// using the given IFormatProvider.
+        /// </summary>
+        /// <param name="fmt">Format string.</param>
+        /// <param name="provider">Format provider.</param>
+        /// <param name="args">Positional arguments.</param>
+        /// <returns></returns>
+        public static string Format(this string fmt, IFormatProvider provider, params object[] args)
+        {
+            return String.Format(provider, fmt, args);
+        }
+
+        #endregion
+
+        #region Dictionary format extensions
+
+        private const string ReservedFormatCharacters = "{},:";
+        private const char OpenBrace = '{';
+
+        public static string Format(this string fmt, IDictionary<string, object> dict)
+        {
+            var builder = new StringBuilder();
+
+            // Conversion table for keys to indexes.
+            var order = new Dictionary<string, int>();
+
+            // Place values in the correct index order.
+            var values = new object[dict.Count];
+
+            var i = 0;
+            foreach (var pair in dict)
+            {
+                order[pair.Key] = i;
+                values[i] = pair.Value;
+                i++;
+            }
+
+            var key = new StringBuilder();
+            var parsingKey = false;
+
+            foreach (var ch in fmt)
+            {
+                if (!parsingKey)
+                {
+                    builder.Append(ch);
+                    if (ch == OpenBrace)
+                    {
+                        parsingKey = true;
+                        continue;
+                    }
+                    if (!ReservedFormatCharacters.Contains(fmt))
+                    {
+                        continue;
+                    }
+                }
+                else if (key.Length == 0 && ch == OpenBrace)
+                {
+                    parsingKey = false;
+                    builder.Append(ch);
+                    continue;
+                }
+                else if (ch == OpenBrace || ReservedFormatCharacters.Contains(ch))
+                {
+                    parsingKey = false;
+                    int idx;
+                    if (!order.TryGetValue(key.ToString().Trim(), out idx))
+                    {
+                        throw new FormatException("Could not find key " + key);
+                    }
+                    key.Clear();
+                    builder.Append(idx);
+                    builder.Append(ch);
+                    continue;
+                }
+                key.Append(ch);
+            }
+
+            return String.Format(builder.ToString(), values);
+        }
+
+        #endregion
+    }
+}
